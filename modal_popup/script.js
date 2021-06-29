d365f56aa93a4be5eae2c34029794a9068060e3d
dmeadows <meadows.david@siemens.com>
2021-06-29 16:23:46 -0400

    finished modal/popup app

---
+++ modal_popup/script.js
@@ -0,0 +1,20 @@
+// Variables
+const modal = document.querySelector(".modal"),
+    btn = document.querySelector(".btn"),
+    close = document.querySelector(".close");
+
+btn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+
+// Open modal
+function openModal(e) {
+    e.preventDefault();
+    modal.style.display = "block";
+}
+
+// Close modal
+function closeModal() {
+    modal.style.display = "none";
+
+}
